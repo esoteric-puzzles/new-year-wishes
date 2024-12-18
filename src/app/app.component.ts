ca62efddf163d59b2515e885b5c0b1e189e75ce7
esoteric puzzles <esoteric@puzzles.com>
2024-12-18 23:03:48 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataLoaderService } from './services/data-loader.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+[x: string]: any;
+  actionButtonClicked = false;
+  dataLoaded = false;
+
+  data: any = null;
+
+  generatedWish: any = {
+    title: "",
+    text: "",
+    image: "",
+  };
+
+  constructor(private dataLoaderService: DataLoaderService) {
+  }
+
+  ngOnInit(): void {
+    this.dataLoaderService.startLoading().subscribe(data => {
+      this.data = data;
+      this.dataLoaded = true;
+      console.log("data", data);
+    });
+  }
+
+  generateWish() {
+    const wishes = this.data.Wishes;
+    const numberOfWishes = Object.keys(this.data.Wishes)?.length;
+
+    this.actionButtonClicked = true;
+    
+    const randomIndex = this.getRandomInt(numberOfWishes);
+    
+    this.generatedWish = {
+      title: 'Предсказание',
+      text: Object.values(wishes)[randomIndex] as string[],
+      image: this.getRandomInt(13) + 1
+    }
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+}
