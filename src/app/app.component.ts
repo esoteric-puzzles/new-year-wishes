ac0f1d53cc2b86065ddcb34d0ce2ebed45ea0fcb
esoteric puzzles <esoteric@puzzles.com>
2024-12-18 23:24:14 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'newyear-wishes';
+  actionButtonClicked = false;
+
+  toggleAction(): void {
+    this.actionButtonClicked = true;
+  }  
 }
